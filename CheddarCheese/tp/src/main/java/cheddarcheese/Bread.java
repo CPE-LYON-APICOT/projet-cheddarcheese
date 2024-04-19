fa60f7ad04b45551d97740631c29e21d26ad2630
Tom REINA-HIOT <t.reinahiot@eleve.leschartreux.net>
2024-04-19 14:40:24 +0200

    Modification de Bread

--- CheddarCheese/tp/src/main/java/cheddarcheese/Bread.java
+++ CheddarCheese/tp/src/main/java/cheddarcheese/Bread.java
@@ -1,7 +1,14 @@
 package cheddarcheese;
 
 public class Bread extends Food {
+    private SlicedTomato slicedTomato;
+    private Steak steak;
+    private Salad salad;
+
     public Bread(int xPos, int yPos) {
         super(xPos, yPos, "Bread");
+        this.slicedTomato = null;
+        this.steak = null;
+        this.salad = null;
     }
 }
