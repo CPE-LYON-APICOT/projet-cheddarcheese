981e7b616c8f775f314e92c3438e8cecc96139b0
LORIS BORJA <loris.borja@gmx.fr>
2024-05-16 09:00:43 +0200

    Verif

--- CheddarCheese/tp/src/main/java/cheddarcheese/Foods/BaseForRecipe.java
+++ CheddarCheese/tp/src/main/java/cheddarcheese/Foods/BaseForRecipe.java
@@ -27,14 +27,23 @@ public class BaseForRecipe extends Food {
 
     // Check if the list of ingredients is complete and if the top is the good one
     public Food checkIngredients(Food topIngredient) {
+        int isRecipe;
+        List<Food> currentIngredients = new ArrayList<>();
+        currentIngredients.addAll(ingredients);
+
+        currentIngredients.remove(currentIngredients.size() - 1);
+
         for (Recette recette : getClass().getAnnotationsByType(Recette.class)) {
             if (recette.top() == topIngredient.getClass()) {
+                isRecipe = 1;
                 List<Class<? extends Food>> recetteIngredients = Arrays.asList(recette.ingredients());
                 List<Class<? extends Food>> ingredientClasses = new ArrayList<>();
-                for (Food ingredient : ingredients) {
+
+                for (Food ingredient : currentIngredients) {
+                    if(!recetteIngredients.contains(ingredient.getClass())) isRecipe = 0;
                     ingredientClasses.add(ingredient.getClass());
                 }
-                if (ingredientClasses.containsAll(recetteIngredients)) {
+                if (ingredientClasses.containsAll(recetteIngredients) && isRecipe == 1) {
                     try {
                         return recette.transformsTo().getDeclaredConstructor(int.class, int.class)
                                 .newInstance(this.getXPos(), this.getYPos());
